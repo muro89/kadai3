16425c7062f12b68c372730df87bb3da060229c8
YukiIshimuroya <122260585+muro89@users.noreply.github.com>
2023-02-16 11:46:54 +0000

    [edit]kadai3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,34 +2,50 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+
+    redirect_to book_path(@book.id)
   end
 
   def show
+    @user = current_user
     @book = Book.find(params[:id])
   end
-
-  def index
-    @books = Book.all
+  
+  def edit
+    @book = Book.find(params[:id])
+  
   end
   
+  def update
+    
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+  
+  def index
+    @user = current_user
+    @books = Book.all
+    @book = Book.new
+  end
+
    def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
    end
-  
-  
+
+
     private
 
   def book_params
     params.require(:book).permit(:title, :image, :body)
   end
 
-  
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
@@ -18,6 +19,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
